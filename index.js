b29c631f7ba32550346a4e14fa66346ef1a85308
MOHAMED ARUSHAM HUSSAIN <TP050300@mail.apu.edu.my>
2021-07-13 00:10:27 +0800

    Sequelize initaited
    Co-authored-by: MOHAMED ARUSHAM HUSSAIN <TP050300@mail.apu.edu.my>

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
- // server.js
- var express = require('express');
- var app = express();
- var port = 8080;
+
+// server.js
+ const express = require('express');
+ const app = express();
+ const port = 8080;
+ const cors = require("cors");
+ 
+
+ // Models and Database
+ const db = require("./models")
+ 
+ // Middlewares 
+  app.use(express.json({limit: "50mb"}));
+  app.use(express.urlencoded({limit: "50mb"}))
  
  // start the server
- app.listen(port, function() {
-   console.log('Sever running on', port);
- });
+ db.sequelize.sync().then((req) => {
+  app.listen(port, function() {
+    console.log('Sever running on', port);
+  })
+ })
+
+ //Routers
+ const CenterRouter = require("./routes/centers")
+ app.use("/centers", CenterRouter)
  
  // route our app
  app.get('/', function(req, res) {
-   res.send('hello world!');
- });
+  res.send('hello world!');
+});
